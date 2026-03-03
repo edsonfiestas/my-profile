00c36ec67352ab61d90cd1b5362fea5eb1fa755a
efiestas <efiestas@quanticotrends.com>
2026-03-03 00:16:48 -0500

    cambio de padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProfileCard from "@/components/ProfileCard";
 
 export default function Home() {
   return (
-    <div className="p-3 sm:p-5">
+    <div className="p-3 sm:p-5 w-full flex justify-center">
       <ProfileCard />
     </div>
   );
